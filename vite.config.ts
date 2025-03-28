93b8d8e6a279d7cc0e59c2eab37bb93eece71f19
Wunderay <60483832+wunderay@users.noreply.github.com>
2025-03-28 02:56:05 -0700

    build: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'fetch_frontend_take_home',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
